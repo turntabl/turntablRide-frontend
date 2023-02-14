7c66db2fce77b4521379c1334e35024e10aff58e
Maxwell Agyapong <maxwell.agyapong@turntabl.io>
2023-02-14 10:38:36 +0000

    Created welcome and login/registration views

---
+++ landing.py
@@ -0,0 +1,36 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+Builder.load_file("kv_files/landing.kv")
+
+
+class WelcomeView(BoxLayout):
+
+    def go_to_login(self):
+        App.get_running_app().root.current = "login"
+
+
+class DashBoardView(BoxLayout):
+    pass
+
+
+class MyApp(App):
+    def build(self):
+        Window.clearcolor = (1, 1, 1, 1)
+        self.screen_manager = ScreenManager()
+
+        self.welcome_screen = Screen(name="welcome")
+        self.welcome_screen.add_widget(WelcomeView())
+        self.screen_manager.add_widget(self.welcome_screen)
+
+        self.login_screen = Screen(name="login")
+        self.login_screen.add_widget(DashBoardView())
+        self.screen_manager.add_widget(self.login_screen)
+        return self.screen_manager
+
+
+MyApp().run()
