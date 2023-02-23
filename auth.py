9d562422520f0b091d55285ce45622a2eefd81fd
Richmond <richmond.tweneboanah@turntabl.io>
2023-02-23 12:43:46 +0000

    Added documentation to code

--- auth.py
+++ auth.py
@@ -9,18 +9,43 @@ from kivymd.app import MDApp
 load_dotenv()
 
 class Authentication:
+    '''
+    This class provides authentication functionality using Google Oauth2.
+
+    Notes
+    -----
+    This class only works with Desktop applications
+    '''
+
     def __init__(self):
         initialize_google(self.after_login, self.error_listener, os.environ["GOOGLE_CLIENT_ID"], os.environ["GOOGLE_CLIENT_SECRET"])
         self.__loading = Loading()
 
     def login(self):
+        '''Method to call to start the login process.'''
+
         self.__loading.open()
         login_google()
 
     def after_login(self, name, email, photo_uri):
+        '''
+        Is called after the login process is successful.
+
+        Parameters
+        ----------
+        name : str
+            The name of the user in Google
+        email : str
+            The google email of the user used to signin 
+        photo_uri : str
+            A url to the google profile of the user
+        '''
+
         self.__loading.dismiss()
         MDApp.get_running_app().root.current = "login"
 
     def error_listener(self):
+        '''Called whenever there is an error in the login process'''
+
         Snackbar(text="Error logging in. Check connection or try again.").open()
         Clock.schedule_once(lambda *args: self.__loading.dismiss())
--- loading.py
+++ loading.py
@@ -3,10 +3,11 @@ from kivymd.uix.button import MDFlatButton
 from kivyauth.utils import stop_login
 from kivy.lang import Builder
 
-
+# used to load the kv file and make it available.
 Builder.load_file("kv_files/loading.kv")
 
 class Loading(MDDialog):
+    '''This is a custom Dialog widget showing a spinner and a button'''
 
     def __init__(self, **kwargs):
         cancel_btn = MDFlatButton(text="CANCEL", on_release=lambda *args: (stop_login(), self.dismiss()))
