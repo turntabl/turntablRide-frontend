b8e95d70b44a1049db64a04aecb1591ad2a2d417
richkode <99684813+richkode@users.noreply.github.com>
2023-03-17 11:42:14 +0000

    TR-7: Create user registration/login implementation (#6)
    * Initial Commit
    
    * Created welcome and login/registration view
    
    * added a kv file
    
    * implemented login
    
    * separating logic and views
    
    * moved loading.kv to kv_files folder
    
    * implemented login feature with the right folder structure
    
    * Added documentation to code
    
    * documented and add oauth package
    
    * fix for server opening only once
    
    * separating configurations
    
    ---------
    
    Co-authored-by: Maxwell Agyapong <maxwell.agyapong@turntabl.io>
    Co-authored-by: Richmond <richmond.tweneboanah@turntabl.io>

---
+++ auth.py
@@ -0,0 +1,79 @@
+from kivymd.uix.snackbar import Snackbar
+from kivy.clock import Clock
+from loading import Loading
+import config
+import threading
+from kivymd.app import MDApp
+from google_auth import GoogleOAuth
+
+
+class Authentication:
+    """
+    This class provides authentication functionality using Google Oauth2.
+
+    Notes
+    -----
+    This class only works with Desktop applications
+    """
+
+    def __init__(self):
+        self.google_login = GoogleOAuth(
+            config.CLIENT_ID,
+            config.CLIENT_SECRET,
+            self.after_login,
+            self.error_listener,
+        )
+        self.login_thread = threading.Thread(target=self.google_login.login)
+        self.login_thread.daemon = True
+        self.loading = Loading(self.google_login.stop_tok_server)
+
+    def login(self):
+        """Method to call to start the login process."""
+        self.loading.open()
+        self.login_thread.start()
+
+    def after_login(self, token):
+        """
+        Is called after the login process is successful.
+
+        Parameters
+        ----------
+        token : str
+            used to access resources on the backend not google resources
+        """
+        import requests
+
+        header = {"Authorization": "Bearer " + token}
+
+        try:
+            resp = requests.get(config.BACKEND_SERVER + "/api/v1/demo", headers=header)
+            root = MDApp.get_running_app().root
+            status_code = resp.status_code
+            if status_code == 200:
+                Clock.schedule_once(lambda *args: (change_screen(root, resp),), 0)
+            elif status_code == 401:
+                Clock.schedule_once(
+                    lambda *args: (
+                        Snackbar(text="You don't have access to this screen").open(),
+                    ),
+                    0,
+                )
+            Clock.schedule_once(lambda *args: (self.loading.dismiss(),), 0)
+        except requests.exceptions.RequestException:
+            self.error_listener("Backend server is down")
+
+    def error_listener(self, msg):
+        """Called whenever there is an error in the login process"""
+
+        Clock.schedule_once(
+            lambda *args: (
+                self.loading.dismiss(),
+                Snackbar(text=msg).open(),
+            ),
+            0,
+        )
+
+
+def change_screen(root, resp):
+    root.current = "dashboard"
+    root.get_screen("dashboard").ids.dashboard.ids.welcome_text.text = resp.text
---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+BACKEND_SERVER = "http://localhost:8080"
