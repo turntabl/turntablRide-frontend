72063fd59e948a3769a8c52a5eaf6015d1da733b
John Paul Kpantey <john.kpantey@turntabl.io>
2023-03-20 05:50:24 +0000

    chore: Add main run file to manage the app screens

---
+++ run.py
@@ -0,0 +1,44 @@
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.screenmanager import MDScreenManager
+from app.utils.colors import Colors
+from app.utils.fonts import Fonts
+
+
+class LoginScreen(MDScreen):
+    pass
+
+
+class DestinationScreen(MDScreen):
+    pass
+
+
+class CarRegistrationScreen(MDScreen):
+    pass
+
+
+class PickupLocationScreen(MDScreen):
+    pass
+
+
+class WindowManager(MDScreenManager):
+    pass
+
+
+class Run(MDApp):
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.COLORS = Colors()
+        self.Fonts = Fonts()
+
+    def on_start(self):
+        Window.size = (360, 640)
+        
+        self.theme_cls.theme_style = "Light"
+
+
+if __name__ == '__main__':
+    Run().run()
