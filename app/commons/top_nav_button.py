53570641272066dabec48fea2ebe92371f7843f8
John Paul Kpantey <john.kpantey@turntabl.io>
2023-03-20 06:34:27 +0000

    chore: Add top nav icon buttons component

---
+++ app/commons/top_nav_button.py
@@ -0,0 +1,10 @@
+from kivy.lang import Builder
+
+Builder.load_file("kiv/commons/top_nav_button.kv")
+
+
+class TopNavIconButton(object):
+    """The class hold all UI components and Logics for the Top nav icon buttons
+
+    """
+    pass
