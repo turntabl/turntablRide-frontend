7d1561d094f44a2bec7ae56712fbad8b358d2a65
John Paul Kpantey <john.kpantey@turntabl.io>
2023-03-20 08:26:39 +0000

    docs: Add comment description

--- run.py
+++ run.py
@@ -5,13 +5,29 @@ from kivymd.uix.screenmanager import MDScreenManager
 from app.utils.colors import Colors
 from app.utils.fonts import Fonts
 
+""" The run.py would contain all the screens of the application as well as the Screen Manager.
+    All new developed screens must be included here. As shown below (LoginScreen class) is included as a screen
+    which would be managed by the WindowManager class.
+    The contents and stylying of the screens goes into app/screens/ and kiv/screens/ respectively
+"""
 
 class LoginScreen(MDScreen):
+    """Holds the login screen of the app
+
+
+    :param MDScreen: A a component of MDScreenManager  
+    :type MDScreen: ~kivymd.uix.screenmanager.MDScreenManager
+    """
     pass
 
 
 
 class WindowManager(MDScreenManager):
+    """ Manages and switches the applications to different screens
+
+    :param MDScreenManager: Screen manager. This is the main class that will control your ~kivymd.uix.screen.MDScreen stack and memory  
+    :type MDScreenManager: ~kivymd.uix.screenmanager.MDScreenManager
+    """
     pass
 
 
