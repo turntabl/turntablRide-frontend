c5e4019a9b93fd89588f8e657ad7aaf74f90485a
John Paul Kpantey <john.kpantey@turntabl.io>
2023-03-23 15:30:48 +0000

    chore: change directory

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
-CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-BACKEND_SERVER = "http://localhost:8080"
