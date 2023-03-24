e1d02eef40c0957b80a1f7c4f21c85953d23dfd0
John Paul Kpantey <john.kpantey@turntabl.io>
2023-03-24 17:50:24 +0000

    feat: Add destination screen

--- app/controller/destination_controller/destination.py
+++ app/controller/destination_controller/destination.py
@@ -1,5 +1,6 @@
 from app.view.commons.toast.toast import Toaster
 from app.utils.colors import Colors
+from kivymd.app import MDApp
 
 class DestinationController(object):
     """Control and validate the user provided destination and return to model if valid 
@@ -8,23 +9,23 @@ class DestinationController(object):
     """
     def __init__(self, destination: str):
         self.destination = destination
+        self.root = MDApp.get_running_app().root
 
-    def get_destination(self):
+    def get_destination(self) -> str:
         return self.destination
 
-    def set_destination(self, destination):
+    def set_destination(self, destination: str) -> None:
         self.destination = destination
 
-    def register_destination(self):
+    def register_destination(self) -> None:
         if self.is_valid_destination():
-            # {"status": "success", "message": {"destination": self.destination}}
-
-            return Toaster(message="Destination is not valid", bg_color=Colors().ErrorColor.get("BackgroundColor"),font_size=14).toast()
+            Toaster(message="Destination is not valid", bg_color=Colors().ErrorColor.get("BackgroundColor"),font_size=14).toast()
              
-        # {"status": "error", "message": "Destination is not valid"}
         # TODO send data to model or cache and proceed to next scrren
-        return Toaster(message= f"destination {self.destination}", bg_color=Colors().SuccessColor.get("BackgroundColor"),font_size=14).toast()
+        self.root.current = "CarRegistrationScreen"
+        self.root.transition.direction = "left"
+        Toaster(message= f"destination {self.destination}", bg_color=Colors().SuccessColor.get("BackgroundColor"),font_size=14).toast()
           
 
-    def is_valid_destination(self):
+    def is_valid_destination(self) -> bool:
         return len(self.destination) == 0
--- app/view/screens/destination_screen/destination_screen.py
+++ app/view/screens/destination_screen/destination_screen.py
@@ -3,8 +3,6 @@ from kivy.properties import ObjectProperty
 from kivymd.uix.screen import MDScreen
 from app.controller.destination_controller.destination import DestinationController
 
-from app.view.commons.toast.toast import Toaster
-from app.utils.colors import Colors
 Builder.load_file("app/view/screens/destination_screen/destination_screen.kv")
 
 class DestinationScreen(MDScreen):
