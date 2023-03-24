4d2290eb81ce4977b110f165f3ba58c0d7f39e9d
John Paul Kpantey <john.kpantey@turntabl.io>
2023-03-24 13:41:36 +0000

    refactor: Change text info and components path

--- app/controller/destination_controller/destination.py
+++ app/controller/destination_controller/destination.py
@@ -4,7 +4,7 @@ from app.utils.colors import Colors
 class DestinationController(object):
     """Control and validate the user provided destination and return to model if valid 
     or back to user if not valid
-    
+
     """
     def __init__(self, destination: str):
         self.destination = destination
@@ -18,9 +18,11 @@ class DestinationController(object):
     def register_destination(self):
         if self.is_valid_destination():
             # {"status": "success", "message": {"destination": self.destination}}
+
             return Toaster(message="Destination is not valid", bg_color=Colors().ErrorColor.get("BackgroundColor"),font_size=14).toast()
              
         # {"status": "error", "message": "Destination is not valid"}
+        # TODO send data to model or cache and proceed to next scrren
         return Toaster(message= f"destination {self.destination}", bg_color=Colors().SuccessColor.get("BackgroundColor"),font_size=14).toast()
           
 
