eac30ddd0b38f2812dae98e4ab302063a5cdc5d7
PRINCEKK92 <129057891+PRINCEKK92@users.noreply.github.com>
2023-03-28 10:46:03 +0000

    Revert "TR-20 Designed the Departure Screen (#15)"
    This reverts commit e907b4b9ab19099f3ac13780d2e2a04161710409.

--- config/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
-CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-BACKEND_SERVER = "http://localhost:8080"
-PREFERRED_WINDOW_SIZE = (360, 640)
--- test/README.md
+++
@@ -1,6 +0,0 @@
-# About test/
-
-The `` test/ `` contain tests that assert that your app runs as expected.
-You may also use this test to run your screen test
-
-Create a sub directory and keep them in
