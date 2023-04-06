b1cb2a744bd384bfbdaca012b4e8aa2b32fc4918
PRINCEKK122 <prince.karikari@turntabl.io>
2023-04-06 15:24:13 +0000

    renamed the screens and py files in riders_screen directory

---
+++ app/view/screens/riders_screen/added_riders_screen.py
@@ -0,0 +1,3 @@
+from kivymd.uix.screen import MDScreen
+from kivy.lang import Builder
+from kivymd.uix
--- app/view/screens/riders_screen/adding_riders_screen.py
+++ app/view/screens/riders_screen/adding_riders_screen.py
@@ -2,7 +2,6 @@ from kivymd.uix.screen import MDScreen
 from kivy.lang import Builder
 from kivymd.uix.floatlayout import MDFloatLayout
 
-Builder.load_file("app/view/screens/riders_screen/riders_screen.kv")
-
-class RidersScreen(MDScreen, MDFloatLayout):
+Builder.load_file("app/view/screens/riders_screen/adding_riders_screen.kv")
+class AddingRidersScreen(MDScreen, MDFloatLayout):
     pass
