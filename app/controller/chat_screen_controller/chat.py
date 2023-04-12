cbf4b03e923c56af190edc4e7cc671512040259e
Maxwell Agyapong <maxwell.agyapong@turntabl.io>
2023-04-12 10:48:25 +0000

    Inserted send button image

--- app/controller/chat_screen_controller/chat.py
+++ app/controller/chat_screen_controller/chat.py
@@ -4,15 +4,16 @@ class ChatScreenController(object):
     """Handles all logic related to the chat screen
     """
     def __init__(self):
-        #self.id = id
         self.root = MDApp.get_running_app().root
         
-    def send_message(self, *args):
-        # Get the current text from the TextInput
-        message = self.message_input.text
-        
-        # Clear the TextInput
-        self.message_input.text = ""
-        
-        # Add the new message to the chat screen
-        self.chat_label.text += f"\nUser: {message}"
+    def send_message(self):
+        message = self.ids.message_input.text
+
+        if message.strip() == "":
+            return
+
+        # Add the message to the chat label
+        self.ids.chat_label.text += f"[b][color=0080ff]You:[/color][/b] {message}\n"
+
+        # Clear the message input field
+        self.ids.message_input.text = ""
