a06522915d18c2c8b906cacb7dfc0fb6c61a6003
Maxwell Agyapong <maxwell.agyapong@turntabl.io>
2023-04-12 10:05:49 +0000

    Added send button

--- app/controller/chat_screen_controller/chat.py
+++ app/controller/chat_screen_controller/chat.py
@@ -1,10 +1,18 @@
-from app.view.commons.toast.toast import Toaster
-from app.utils.colors import Colors
 from kivymd.app import MDApp
 
 class ChatScreenController(object):
     """Handles all logic related to the chat screen
     """
-    def __init__(self, destination):
-        self.destination = destination
+    def __init__(self):
+        #self.id = id
         self.root = MDApp.get_running_app().root
+        
+    def send_message(self, *args):
+        # Get the current text from the TextInput
+        message = self.message_input.text
+        
+        # Clear the TextInput
+        self.message_input.text = ""
+        
+        # Add the new message to the chat screen
+        self.chat_label.text += f"\nUser: {message}"
