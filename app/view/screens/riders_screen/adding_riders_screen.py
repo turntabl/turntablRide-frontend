c024224153e615ab0be5105f9bf603a0362fe631
PRINCEKK122 <prince.karikari@turntabl.io>
2023-04-20 00:12:00 +0000

    design list of riders section

--- app/view/screens/riders_screen/adding_riders_screen.py
+++ app/view/screens/riders_screen/adding_riders_screen.py
@@ -1,13 +1,18 @@
 import os
 from kivy.properties import StringProperty
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDTextButton
 from kivy.lang import Builder
+from kivy.core.window import Window
 
 dir = os.path.dirname(__file__)
 Builder.load_file(os.path.join(dir, "adding_riders_screen.kv"))
+
 
 class AddingRidersScreen(MDScreen):
     driver_name = StringProperty("Kelvin Mills")
     
     def open_riders_section(self):
+        print(self.ids)
         print("Connecting to the riders section...")
